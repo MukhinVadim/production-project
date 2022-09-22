3198f550056765540b3510b04c7319b5c3006e11
Vadim Mukhin <vadim.mukhin@intravision.ru>
2022-09-22 22:33:21 +0300

    Link fix

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,20 +1,21 @@
 import classNames from "classnames"
-import { Link, LinkProps } from "react-router-dom"
+import { Link, LinkProps, NavLink, NavLinkProps } from "react-router-dom"
 import cls from './AppLink.module.scss'
 
 type AppLinkProps = {
     className?: string;
-} & LinkProps
+} & NavLinkProps
 
 export const AppLink: React.FC<AppLinkProps> = (props) => {
 
   const {className, children, ...restProps} = props;
   return (
-    <Link
+    <NavLink
       {...restProps}
-      className={classNames(cls.AppLink, className)}
+      className={({isActive}) => classNames(cls.AppLink, className, {[cls.isActive]: isActive})}
+      end
      >
       { children }
-    </Link>
+    </NavLink>
   )
 }
