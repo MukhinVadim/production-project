00ab9b78b27039953f554328b9fbc28c28c584d0
Vadim <77540337+MukhinVadim@users.noreply.github.com>
2022-10-14 12:50:03 +0300

    Delete src/widgets/Sidebar directory

--- src/widgets/Sidebar/ui/Sidebar.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Sidebar } from './Sidebar';
-
-export default {
-  title: 'Widgets/Sidebar',
-  component: Sidebar,
-} as ComponentMeta<typeof Sidebar>;
-
-export const Primary: ComponentStory<typeof Sidebar> = () => <Sidebar />;
