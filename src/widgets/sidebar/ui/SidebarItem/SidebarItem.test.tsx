011b27becad3f0a2bdb5b9149ab2df6be3cfc433
MukhinVadim <mukhinvadim@gmail.com>
2022-10-29 18:11:25 +0300

    tests updated

--- src/widgets/sidebar/ui/SidebarItem/SidebarItem.test.tsx
+++ src/widgets/sidebar/ui/SidebarItem/SidebarItem.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { componentRender } from '@config/jest/componentRender';
+import { screen } from '@testing-library/react';
 import { SidebarItem } from './SidebarItem';
 
 describe('SidebarItem', () => {
   test('Test render', () => {
-    render(<SidebarItem to="/">link</SidebarItem>);
-    expect(screen.getByTestId('sidebar-item')).toBeInTheDocument();
+    componentRender(<SidebarItem to="/">link</SidebarItem>);
+    expect(screen.getByText('link')).toBeInTheDocument();
   });
 });
